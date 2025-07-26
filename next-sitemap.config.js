e63a3340a05d21a32eb79d1f34cf29f70e2e2b0a
Tony Youssef <151008222+DeadCode-Dev@users.noreply.github.com>
2025-07-26 06:03:23 +0000

    Remove StructuredData component as structured data has been migrated to the metadata object in the layout file.

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://antonious.me',
+  generateRobotsTxt: false,
+  generateIndexSitemap: false,
+};
---
+++ src/components/StructuredData.tsx
@@ -0,0 +1,1 @@
+// This file is no longer needed. Structured data has been moved to the metadata object in your layout file.
